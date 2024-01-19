a8dc1f593aadd1d4ba6a44b4988ef8fa525adc0d
AntonW <awiklund76@gmail.com>
2024-01-19 10:43:52 +0200

    Add karma-junit-reporter plugin to Jenkinsfile

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,18 +1,22 @@
 // karma.conf.js
 module.exports = function(config) {
     config.set({
-      // ... other configuration ...
-  
-      reporters: ['progress', 'junit'],
-  
-      // JUnit Reporter configurations
-      junitReporter: {
-        outputFile: 'test-results.xml', // Output file
-        useBrowserName: false, // Add browser name to report and classes names
-        suite: '', // Suite will become the package name attribute in xml testsuite element
-      },
-  
-      // ... rest of the configuration ...
+        // ... other configuration ...
+
+        reporters: ['progress', 'junit'],
+
+        // JUnit Reporter configurations
+        junitReporter: {
+            outputFile: 'test-results.xml', // Output file
+            useBrowserName: false, // Add browser name to report and classes names
+            suite: '', // Suite will become the package name attribute in xml testsuite element
+        },
+
+        plugins: [
+            'karma-junit-reporter',
+            // ... other plugins you might be using ...
+        ],
+
+        // ... rest of the configuration ...
     });
-  };
-  
+};
